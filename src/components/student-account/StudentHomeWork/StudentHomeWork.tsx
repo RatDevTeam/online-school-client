61e138a7bc436141ce04e14e9a96b16bc9a9a199
plastic-flomaster <nastyapl98@gmail.com>
2020-07-26 14:31:00 +0500

    improvements/добавлен url-plugin +  robots + prettier

---
+++ src/components/student-account/StudentHomeWork/StudentHomeWork.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface IStudentHomeWork {}
+
+const StudentHomeWork: React.FC<IStudentHomeWork> = () => {
+	return <div>H</div>;
+};
+
+export default StudentHomeWork;
---
+++ src/components/student-account/StudentScripts/StudentScripts.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface IStudentScripts {}
+
+const StudentScripts: React.FC<IStudentScripts> = () => {
+	return <div>H</div>;
+};
+
+export default StudentScripts;
