eb036ec982afabdde11f1b2a2f73684cf97ee4e3
plastya <nastyapl98@gmail.com>
2020-08-21 02:05:20 +0500

    reset url

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { Store } from './redux/store';
 import createRouter from './router/create-router';
 import App from './App';
 
-axios.defaults.baseURL = process.env.URL_API;
+axios.defaults.baseURL = 'https://likbez-school.ru/api';
 
 axios.interceptors.request.use(
 	(request) => {
