8ce7773f16cd22ca3cb2f6e943840541f19c149f
plastya <nastyapl98@gmail.com>
2020-08-28 21:37:32 +0500

    update webpack

--- src/tests/__tests__/Landing.test.jsx
+++ src/tests/__tests__/Landing.test.jsx
@@ -3,6 +3,6 @@ import renderer from 'react-test-renderer';
 import LandingPage from '../../components/landing-page/LandingPage/LandingPage';
 
 it('корректно рендерит лендинг ликбез', () => {
-	const tree = renderer.create(<LandingPage/>).toJSON();
+	const tree = renderer.create(<LandingPage />).toJSON();
 	expect(tree).toMatchSnapshot();
 });
